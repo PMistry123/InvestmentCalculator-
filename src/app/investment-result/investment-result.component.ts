8097910f4234c655658f9e3bb020890ca660ac58
Pankti Pancholi <panktipancholi@mac.home>
2026-04-08 02:48:50 -0400

    Add code files

---
+++ src/app/investment-result/investment-result.component.ts
@@ -0,0 +1,17 @@
+import { Component, computed, inject } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+
+import { InvestmentService } from '../investment-service';
+
+@Component({
+  selector: 'app-investment-result',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-result.component.html',
+  styleUrl: './investment-result.component.css'
+})
+export class InvestmentResultComponent {
+  private investmentService = inject(InvestmentService);
+
+  results = computed(() => this.investmentService.resultsData());
+}
